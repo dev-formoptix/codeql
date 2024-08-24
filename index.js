8c8dd8d765c19a6e1db96d043915d2b4b3a5c39d
rahulcoder010 <139946269+rahulcoder010@users.noreply.github.com>
2024-08-24 18:29:21 +0530

    FO-5 (bug) Uncontrolled command line

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const mysql = require('mysql');
 const rateLimit = require('express-rate-limit');
 const app = express();
 const port = 3000;
+import shellQuote from 'shell-quote';
 
 // Sample MySQL connection setup
 const connection = mysql.createConnection({
@@ -38,7 +39,7 @@ app.get('/users/:id', (req, res) => {
 // Command Injection vulnerability (Alert 2)
 app.get('/exec', (req, res) => {
     const cmd = req.query.command;
-    const args = cmd.split(' ');
+    const args = shellQuote.parse(cmd);
 
     execFileSync(args[0], args.slice(1), { shell: false }); // Using execFileSync with array of arguments instead of splitting cmd
 
